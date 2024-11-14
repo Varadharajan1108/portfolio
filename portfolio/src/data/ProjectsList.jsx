3225cb6625cfa0de49eb10a7bc5f092faca5c8ef
Varadharajan <varadhu1108@gmail.com>
2024-11-14 22:53:50 +0530

    push

---
+++ portfolio/src/data/ProjectsList.jsx
@@ -0,0 +1,31 @@
+export const ProjectsList = {
+  projects: [
+    {
+      name: "Metadata App",
+      description:
+        "An application within the salesforce org that displays metadata values of various objects and their relationships",
+      technologies: [
+        "Apex",
+        "Salesforce",
+        "SOAP Api",
+        "REST Api",
+        "Visualforce",
+        "Lightning Web Components",
+        "JavaScript",
+        "HTML",
+        
+      ],
+    },
+    
+    {
+      name: "Personal Portfolio",
+      description:
+        "My portfolio is a website built with React.js and Tailwind CSS, utilizing React Router v6 to enable easy access to multiple pages. It was designed and created entirely by myself, showcasing my skills and projects. The website is responsive and visually appealing, providing a user-friendly experience.",
+      technologies: ["React JS", "Tailwind CSS", "Git", "GitHub", "Figma"],
+    },
+    
+    
+    
+    
+  ],
+};
---
+++ portfolio/src/data/UserData.jsx
@@ -0,0 +1,38 @@
+export const UserData = {
+  name: "Varadharajan",
+  about : "I am a self-taught front-end developer specializing in building dynamic, accessible websites that function seamlessly across all devices. My expertise lies in React.js, Redux, JavaScript, HTML5, and CSS. I am passionate about crafting creative solutions to enhance user experiences and excel at code management and collaboration using Git version control. Beyond web development, I have explored UI/UX design using Figma, and I have hands-on experience in Salesforce administration, having earned my Salesforce Certified Administrator certification. My approach to development is driven by a deep commitment to exceeding client expectations with high-quality solutions, including my work on mini-program apps. I continuously strive to learn and apply my skills in both technology and business domains to achieve impactful outcomes.",
+  resumeUrl : "https://drive.google.com/file/d/1B8qW6DkYzwPlyJbyUfETjf6adhk1J-g_/view?usp=drive_link",
+  FooterLink : "https://www.linkedin.com/in/varadharajan1108/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/Varadharajan1108",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/varadharajan1108/",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "twitter",
+      url: "https://twitter.com/varadharajan_1108",
+      icon: "AiOutlineTwitter",
+    },
+    {
+      socialMediaName: "instagram",
+      url: "https://instagram.com/varadharajan_1108",
+      icon: "AiFillInstagram",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Sales and Marketing",
+      "Prompt Engineering",
+      "Salesforce Administrator",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
